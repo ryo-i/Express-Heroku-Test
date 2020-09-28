f6b8dd6fd00323142143063b2e15d82ab9cd5006
Ryo Iida <iidazz0109@gmail.com>
2020-09-29 05:59:38 +0900

    環境変数テストを追加

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(countHitsuji()))
+  // .get('/env', (req, res) => res.send(process.env.TIMES))
+  .get('/env', (req, res) => res.send(process.env.NAME))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  countHitsuji = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      // result += i + ' '
+      result += '羊が' + i + '匹 '
+    }
+    return result;
+  }
